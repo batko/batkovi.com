8b524cce3e14a931fa3c16924b9b23d5cc72b1c3
pavel@batko.cz <pavel@batko.cz>
2015-06-23 20:47:32 +0200

    pass

--- app/model/UserManager.php
+++ app/model/UserManager.php
@@ -38,24 +38,25 @@ class UserManager extends Nette\Object implements Nette\Security\IAuthenticator
 	public function authenticate(array $credentials)
 	{
 		list($username, $password) = $credentials;
-
-		$row = $this->database->table(self::TABLE_NAME)->where(self::COLUMN_NAME, $username)->fetch();
-
-		if (!$row) {
-			throw new Nette\Security\AuthenticationException('The username is incorrect.', self::IDENTITY_NOT_FOUND);
-
-		} elseif (!Passwords::verify($password, $row[self::COLUMN_PASSWORD_HASH])) {
-			throw new Nette\Security\AuthenticationException('The password is incorrect.', self::INVALID_CREDENTIAL);
-
-		} elseif (Passwords::needsRehash($row[self::COLUMN_PASSWORD_HASH])) {
-			$row->update(array(
-				self::COLUMN_PASSWORD_HASH => Passwords::hash($password),
-			));
-		}
-
-		$arr = $row->toArray();
-		unset($arr[self::COLUMN_PASSWORD_HASH]);
-		return new Nette\Security\Identity($row[self::COLUMN_ID], $row[self::COLUMN_ROLE], $arr);
+//
+//		$row = $this->database->table(self::TABLE_NAME)->where(self::COLUMN_NAME, $username)->fetch();
+//
+//		if (!$row) {
+//			throw new Nette\Security\AuthenticationException('The username is incorrect.', self::IDENTITY_NOT_FOUND);
+//
+//		} elseif (!Passwords::verify($password, $row[self::COLUMN_PASSWORD_HASH])) {
+//			throw new Nette\Security\AuthenticationException('The password is incorrect.', self::INVALID_CREDENTIAL);
+//
+//		} elseif (Passwords::needsRehash($row[self::COLUMN_PASSWORD_HASH])) {
+//			$row->update(array(
+//				self::COLUMN_PASSWORD_HASH => Passwords::hash($password),
+//			));
+//		}
+//
+//		$arr = $row->toArray();
+//		unset($arr[self::COLUMN_PASSWORD_HASH]);
+                
+		return new Nette\Security\Identity(1, "guest");
 	}
 
 
