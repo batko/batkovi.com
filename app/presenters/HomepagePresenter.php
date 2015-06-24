584c3ba66f193aad1845cf523b5ae3ef5c6115b7
pavel@batko.cz <pavel@batko.cz>
2015-06-24 18:07:07 +0200

    mailer, contact form

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,6 +10,9 @@ use Nette,
  */
 class HomepagePresenter extends BasePresenter {
 
+    /** @var Nette\Mail\IMailer */
+    private $mailer;
+
     public function renderDefault() {
         $this->template->anyVariable = 'any value';
     }
@@ -25,8 +28,38 @@ class HomepagePresenter extends BasePresenter {
     public function renderPhoto() {
         $this->template->anyVariable = 'any value';
     }
-      public function renderLocation() {
+
+    public function renderLocation() {
         $this->template->anyVariable = 'any value';
+    }
+
+    protected function createComponentFormContact() {
+        $form = new Nette\Application\UI\Form();
+        $form->addText("name");
+        $form->addText("email");
+        $form->addText("phone");
+        $form->addText("text");
+
+        $form->addSubmit("submit");
+
+        $form->onSuccess[] = $this->formContactSubmitted;
+
+        return $form;
+    }
+
+    public function formContactSubmitted($form, $fd) {
+        $latte = new Latte\Engine;
+        $params = array(
+            'text' => $fd->text,
+        );
+
+        $mail = new Nette\Mail\Message;
+        $mail->setFrom($fd->name . " <" . $fd->email . ">")
+                ->addTo('pavel@batko.cz')
+                ->setSubject('Svatba ' . $fd->phone)
+                ->setHtmlBody($latte->renderToString('../app/presenters/template/email.latte', $params));
+
+        //  $this->mailer->send($mail);
     }
 
 }
